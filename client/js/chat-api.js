1f2f3d1dcffeac0af1aa29a9008b962ff394e872
Jordan Kasper <jordan@jordankasper.com>
2016-07-10 12:55:18 -0400

    Update jQuery; rename chat-api init method; remove useless css dir; update html forms

--- client/js/chat-api.js
+++ client/js/chat-api.js
@@ -1,10 +1,16 @@
 
-(function(chat) {
-    chat = chat || (window.chat = {});
+(function(ns) {
+    window.chat = ns = (ns || {});
 
-    chat.init = function initChat(messageHandler) {
+    /**
+     * This initializes the chat client and will call the provided message
+     * handler any time a new message is received from the server.
+     *
+     * @param  {Function} messageHandler  The function to execute when a new message is received.
+     * @return {void}
+     */
+    ns.listenForMessages = function listenForMessages(messageHandler) {
         var socket = io();
-
         messageHandler = messageHandler || (function() {});
         socket.on('message', messageHandler);
     };
